25c68eecba1bd75c763f03ce31f8115b339fc9a1
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2017-04-05 20:46:52 +0200

    Napravljena Flota i test

---
+++ PotapanjeBrodova/PotapanjeBrodova/Flota.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Flota
+    {
+        public void dodajBrod(IEnumerable<Polje> polja)
+        {
+            brodovi.Add(new PotapanjeBrodova.Brod(polja));
+        }
+
+        public int brojBrodova
+        {
+            get { return brodovi.Count; }
+        }
+
+        private List<Brod> brodovi = new List<Brod>();
+    }
+}
