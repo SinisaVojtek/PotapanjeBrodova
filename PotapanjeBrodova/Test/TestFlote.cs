e3d1e0ae45fb0dd3cbb5129305e8cda738c579cd
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2017-04-26 20:41:31 +0200

    Flota implementira sučelje iGađani, napravljeni testovi

--- PotapanjeBrodova/Test/TestFlote.cs
+++ PotapanjeBrodova/Test/TestFlote.cs
@@ -15,5 +15,35 @@ namespace Test
             flota.dodajBrod(new Polje[] { new Polje(1, 1), new Polje(1, 2) });
             Assert.AreEqual(1, flota.brojBrodova);
         }
+
+        [TestMethod]
+        public void Flota_GađajVračaPromašajZaPoljeKojeNePripadaNitiJednomBrodu()
+        {
+            Flota flota = new Flota();
+            flota.dodajBrod(new Polje[] { new Polje(1, 1), new Polje(1, 2) });
+            flota.dodajBrod(new Polje[] { new Polje(5, 6), new Polje(6, 6) });
+            Assert.AreEqual(RezultatGađanja.Promašaj, flota.Gađaj(new Polje(1, 6)));
+
+        }
+
+        [TestMethod]
+        public void Flota_GađajVračaPogodakZaPrvoPogođenoPoljeBroda()
+        {
+            Flota flota = new Flota();
+            flota.dodajBrod(new Polje[] { new Polje(1, 1), new Polje(1, 2) });
+            flota.dodajBrod(new Polje[] { new Polje(5, 6), new Polje(6, 6) });
+            Assert.AreEqual(RezultatGađanja.Pogodak, flota.Gađaj(new Polje(5, 6)));
+        }
+
+        [TestMethod]
+        public void Flota_GađajVračaPotopljenZaPrviPotopljeniBrod()
+        {
+            Flota flota = new Flota();
+            flota.dodajBrod(new Polje[] { new Polje(1, 1), new Polje(1, 2) });
+            flota.dodajBrod(new Polje[] { new Polje(5, 6), new Polje(6, 6) });
+            Assert.AreEqual(RezultatGađanja.Pogodak, flota.Gađaj(new Polje(5, 6)));
+            Assert.AreEqual(RezultatGađanja.Potopljen, flota.Gađaj(new Polje(6, 6)));
+        }
+
     }
 }
