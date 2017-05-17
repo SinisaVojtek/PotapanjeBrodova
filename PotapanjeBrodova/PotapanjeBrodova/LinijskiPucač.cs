de2eb8e0f29bef6e488794749f9e39205400178f
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2017-05-17 20:59:51 +0200

    Dodan TestLinijskogPucača i napravljene izmjene na topništvu

--- PotapanjeBrodova/PotapanjeBrodova/LinijskiPucač.cs
+++ PotapanjeBrodova/PotapanjeBrodova/LinijskiPucač.cs
@@ -7,9 +7,6 @@ namespace PotapanjeBrodova
 {
     public class LinijskiPucač : IPucač
     {
-        Mreža mreža;
-        IEnumerable<Polje> pogođenaPolja;
-        int duljinaBroda;
 
         public LinijskiPucač(Mreža mreža, IEnumerable<Polje> pogođeno, int duljinaBroda)
         {
@@ -28,12 +25,47 @@ namespace PotapanjeBrodova
 
         public Polje Gađaj()
         {
-            throw new NotImplementedException();
+            var kandidati = DajKandidate();
+            return kandidati[izbornik.Next(kandidati.Count())];
         }
 
         public void ObradiGađanje(RezultatGađanja rezultat)
         {
             throw new NotImplementedException();
         }
+
+        private List<Polje> DajKandidate()
+        {
+            if (pogođenaPolja.First().Redak == pogođenaPolja.Last().Redak)
+                return DajHorizontalnaPolja();
+
+            return DajVertikalnaPolja();
+        }
+
+        List<Polje> DajHorizontalnaPolja()
+        {
+            List<Polje> polja = new List<Polje>();
+            Polje prvo = pogođenaPolja.First();
+            Polje zadnje = pogođenaPolja.Last();
+            var LijevaPolja = mreža.DajNizSlobodnihPolja(prvo,Smjer.Lijevo);
+            if (LijevaPolja.Count() > 0)
+                polja.Add(LijevaPolja.First());
+            var DesnaPolja = mreža.DajNizSlobodnihPolja(prvo, Smjer.Desno);
+            if (DesnaPolja.Count() > 0)
+                polja.Add(DesnaPolja.First());
+            return polja;
+        }
+
+        List<Polje> DajVertikalnaPolja()
+        {
+            List<Polje> polja = new List<Polje>();
+            return polja;
+        }
+
+
+        Mreža mreža;
+        IEnumerable<Polje> pogođenaPolja;
+        int duljinaBroda;
+        private Random izbornik=new Random();
     }
 }
